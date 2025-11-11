8f5345ea6e0d0f93ea8d727477a6e7b8984946e9
fadyashraf8 <fadyashraf909@gmail.com>
2025-11-11 12:37:23 +0200

    comment app.all

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { AppError } from "./utils/AppError";
-import { globalMiddleWare } from "./middleware/globalMiddleWare";
+import { AppError } from "./utils/AppError.js";
+import { globalMiddleWare } from "./middleware/globalMiddleWare.js";
 const app = express();
 const port = 3000;
 
-app.all("*", (req, res, next) => {
-  next(new AppError(`can't find this route: ${req.originalUrl}`, 404));
-});
+// app.all("*", (req, res, next) => {
+//   next(new AppError(`can't find this route: ${req.originalUrl}`, 404));
+// });
 
 app.use(globalMiddleWare);
 app.get("/", (req, res) => res.send("Hello World!"));
