29abf2589a584dee6979418e5195a280929dacff
Zeyad Mohamed <ziadmohomar123@gmail.com>
2025-11-21 14:04:07 +0200

    fix: activity creation function

--- controllers/activity.controller.js
+++ controllers/activity.controller.js
@@ -1,3 +1,4 @@
+import { sendEmail } from "../emails/user.email.js";
 import { catchAsyncError } from "../middleware/catchAsyncError.js";
 import { getAllActivitiesForUser, createActivityForUser, deleteActivityById} from "../repository/activity.repository.js";
 import { ApiFeatures } from "../utils/ApiFeatures.js";
@@ -35,32 +36,16 @@ const deleteActivity = catchAsyncError(async (req, res, next) => {
     return res.status(200).send(deletedActivity);
 })
 
-const logActivity = async (userId, action, resourceData) => {
-    let activityText = "";
-
-    switch (action) {
-        case 'POST':
-            activityText = `created a new item`;
-            break;
-        case 'PUT':
-        case 'PATCH':
-            activityText = `updated an item`;
-            break;
-        case 'DELETE':
-            activityText = `deleted an item`;
-            break;
-        default:
-            activityText = `performed an action`;
-    }
-
+const logActivity = async (user, activityText, activityDescription, sendMail=false) => {
     try {
         const activity = {
-            userId: userId,
+            userId: user._id,
             text: activityText, 
-            description: resourceData.name || resourceData.description || activityText
+            description: activityDescription
         }
-        
-        await createActivityForUser(activity); 
+        await createActivityForUser(activity);
+        sendMail ? sendEmail(user.email, activityText, "<p>" + activityDescription + "</p>") 
+            : null; 
     } catch (error) {
         console.error("creating activity failed:", error.message);
     }
--- repository/activity.repository.js
+++ repository/activity.repository.js
@@ -1,13 +1,13 @@
-import {activityModel} from "../models/activity.js";
+import { activityModel } from "../models/activity.js";
 import mongoose from "mongoose";
-export const getAllActivitiesForUser = async (userId) => {
+export const getAllActivitiesForUser = (userId) => {
     if (!mongoose.Types.ObjectId.isValid(userId)) {
         throw new Error("Invalid user ID");
     }
-    return await activityModel.find({userId: userId});
+    return activityModel.find({ userId: userId });
 }
 
-export const createActivityForUser = async (activity)=> {
+export const createActivityForUser = async (activity) => {
     return await activityModel.create(activity);
 }
 
