6990c14ff35ba21012e84d6e6155a5546a36967d
fadyashraf8 <fadyashraf909@gmail.com>
2025-12-08 22:22:20 +0200

    fix in transaction model

--- models/transaction.js
+++ models/transaction.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 const transactionedVocuherSchema = new mongoose.Schema({
   code: { type: String, required: true },        
   discount: { type: Number, required: true },   
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  admin: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, 
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+  admin: { type: mongoose.Schema.Types.ObjectId, ref: "user" }, 
   decision: { type: String, enum: ["accept", "reject"], required: true },
   redeemedAt: { type: Date, default: Date.now },
-  gemId: { type: mongoose.Schema.Types.ObjectId, ref: "Gem" },
+  gemId: { type: mongoose.Schema.Types.ObjectId, ref: "gem" },
 });
 
 export const transactionVoucherModel = mongoose.model("TransactionedVoucher", transactionedVocuherSchema);
